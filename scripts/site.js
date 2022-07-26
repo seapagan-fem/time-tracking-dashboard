0f7105148c6d93b92d2869b564dacad95269aff0
Grant Ramsay <seapagan@gmail.com>
2022-07-26 09:58:19 +0100

    implement selector change
    use styled radio buttons instead of list for the 3 selections

--- scripts/site.js
+++ scripts/site.js
@@ -1,1 +1,10 @@
 console.log("Javascript Loaded");
+
+const handleRadioClick = (e) => {
+  const choice = document.querySelector('input[name="period"]:checked').value;
+  console.log(`Selection Changed to ${choice}`);
+};
+
+document
+  .getElementsByName("period")
+  .forEach((el) => el.addEventListener("click", handleRadioClick));
